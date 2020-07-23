c418611ea2494d5795ba498719d4fd9a5c3b5372
bgpark <bgpark82@gmail.com>
2020-07-23 20:21:44 +0900

    feat : Display user profile on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Styled from 'styled-components/native';
+import {Text, Image} from 'react-native';
+import axios from 'axios';
+
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const Container = Styled.View`
-  height: 40px;
-  flex:1;
-  justify-content: center;
-  align-items: center;
-  background-color: #EEE;
+    flex: 1;
 `;
 
-const Label = Styled.Text`
-  font-weight: bold;
+const Profile = Styled.Image`
+ height: 70;
+    width: 70;
+    borderRadius: 75;
+    borderWidth: 3;
+    borderColor: rgb(255,255,255);
 `;
 
-interface Props {}
+const GoogleMap = () => {
+  const [user, setUser] = useState({});
 
-const App = ({}: Props) => {
+  useEffect(() => {
+    axios.get('http://13.209.217.56/api/v1/user').then((response) => {
+      console.log(response);
+      setUser(response.data.data[0]);
+    });
+  }, []);
+
   return (
     <Container>
-      <Label>Hello</Label>
+      <MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider={PROVIDER_GOOGLE}>
+        <Marker
+          coordinate={{latitude: 37.78825, longitude: -122.4324}}
+          title="this is a marker"
+          description="this is a marker example">
+          <Profile source={{url: user.profile}} />
+        </Marker>
+      </MapView>
     </Container>
   );
 };
 
-export default App;
+export default GoogleMap;
